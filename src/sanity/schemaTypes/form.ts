5f6312eeff6c5ed093d24ac740e81f1d556bd770
Doug Leinen <daleinen@gmail.com>
2025-02-11 09:34:13 -0600

    revert to hardcoded

--- src/sanity/schemaTypes/form.ts
+++ src/sanity/schemaTypes/form.ts
@@ -6,80 +6,9 @@ export const form = defineType({
   type: 'object',
   fields: [
     defineField({
-      name: 'headline',
-      title: 'Headline',
+      name: 'title',
+      title: 'Title',
       type: 'string',
-    }),
-    defineField({
-      name: 'name',
-      title: 'Name',
-      type: 'string',
-      description: 'This field should match the form name in Netlify',
-    }),
-    defineField({
-      name: 'formFields',
-      title: 'Form Fields',
-      type: 'array',
-      of: [
-        {
-          type: 'object',
-          fields: [
-            defineField({
-              name: 'label',
-              title: 'Label',
-              type: 'string',
-            }),
-            defineField({
-              name: 'type',
-              title: 'Type',
-              type: 'string',
-              options: {
-                list: [
-                  { title: 'Text', value: 'text' },
-                  { title: 'Email', value: 'email' },
-                  { title: 'Phone', value: 'tel' },
-                  { title: 'Number', value: 'number' },
-                  { title: 'Date', value: 'date' },
-                  { title: 'Textarea', value: 'textarea' },
-                  { title: 'Checkbox', value: 'checkbox' },
-                  { title: 'Radio', value: 'radio' },
-                  { title: 'Select', value: 'select' },
-                ],
-              },
-              initialValue: 'text',
-            }),
-            defineField({
-              name: 'required',
-              title: 'Required',
-              type: 'boolean',
-            }),
-            defineField({
-              name: 'options',
-              title: 'Options',
-              type: 'array',
-              of: [
-                {
-                  type: 'object',
-                  fields: [
-                    defineField({
-                      name: 'label',
-                      title: 'Label',
-                      type: 'string',
-                    }),
-                    defineField({
-                      name: 'value',
-                      title: 'Value',
-                      type: 'string',
-                    }),
-                  ],
-                },
-              ],
-              hidden: ({ parent }) =>
-                parent?.type !== 'radio' && parent?.type !== 'select',
-            }),
-          ],
-        },
-      ],
     }),
   ],
 });

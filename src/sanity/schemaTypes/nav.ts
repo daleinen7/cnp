e573857d8aa0d145322dee7ceb736954b7d46c86
Doug Leinen <daleinen@gmail.com>
2025-03-01 13:57:58 -0600

    add instructions to preview

--- src/sanity/schemaTypes/nav.ts
+++ src/sanity/schemaTypes/nav.ts
@@ -13,6 +13,8 @@ export default {
       name: 'navItems',
       title: 'Navigation Items',
       type: 'array',
+      description:
+        'pre-append a "/" to the slug if the link is internal; e.g. "/about" otherwise use the full https://www.example.com link to go to an external page',
       of: [{ type: 'navItem' }],
     },
   ],

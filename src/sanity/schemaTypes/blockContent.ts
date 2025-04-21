7de651c4b7b12912407a90e3f4c80e578b7d0608
Doug Leinen <daleinen@gmail.com>
2025-04-21 17:13:29 -0500

    ordered lists; contact not 100%;

--- src/sanity/schemaTypes/blockContent.ts
+++ src/sanity/schemaTypes/blockContent.ts
@@ -31,7 +31,10 @@ export const blockContent = defineType({
         { title: 'H4', value: 'h4' },
         { title: 'Quote', value: 'blockquote' },
       ],
-      lists: [{ title: 'Bullet', value: 'bullet' }],
+      lists: [
+        { title: 'Bullet', value: 'bullet' },
+        { title: 'Numbered', value: 'number' },
+      ],
       // Marks let you mark up inline text in the Portable Text Editor
       marks: {
         // Decorators usually describe a single property – e.g. a typographic
